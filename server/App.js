05eb3c242f5872af1c5adeb39f26354859db287e
Lisha Wang <lishawang1020@gmail.com>
2020-10-23 07:40:43 -0700

    project finished

---
+++ server/App.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const checkAuth = require('./middleware/check-auth');
+
+mongoose.connect('mongodb://127.0.0.1/videoServer', {
+  useCreateIndex: true, 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use('/api/videos', express.static('media/uploads'));
+
+// Routes
+app.use('/api/signUp', require('./router/signUp'));
+app.use('/api/signIn', require('./router/signIn'));
+app.use('/api/upload', checkAuth, require('./router/upload'));
+app.use('/api/videoList', checkAuth, require('./router/videoList'));
+
+module.exports = app;
