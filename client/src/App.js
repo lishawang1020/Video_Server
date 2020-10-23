7ec89a1b3937074cf5325963ca8fe4fe502fffa8
Lisha Wang <lishawang1020@gmail.com>
2020-10-23 07:45:51 -0700

    add client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignIn from './components/Form/SignIn/SignIn';
+import SignUp from './components/Form/SignUp/SignUp';
+import SignOut from './components/SignOut/SignOut';
+import Upload from './components/Upload/Upload';
+import VideoPlayer from './components/VideoPlayer/VideoPlayer';
+
+
+function App() {
+  return (
+    <React.Fragment>
+    <Route exact path="/" component={Dashboard} />
+    <Route exact path="/video/:videoTitle" component={VideoPlayer} />
+    <Route exact path="/signIn" component={SignIn} />
+    <Route exact path="/signUp" component={SignUp} />
+    <Route exact path="/upload" component={Upload} />
+    <Route exact path="/signOut" component={SignOut} />
+    </React.Fragment>
+  );
+}
+
+export default App;
